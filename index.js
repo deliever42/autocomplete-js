74dfa82b72facec12bd0c0fe30c281a3a09dc9b4
Emirhan <77491112+deliever42@users.noreply.github.com>
2022-09-26 14:44:40 +0300

    language correction

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const strings = ["apple", "banana", "orange"];
 const selected = "aplle";
 
-const result = {};
+const results = {};
 
 for (let i = 0; i < strings.length; i++) {
-  const string = strings.at(i);
+  const str = strings.at(i);
 
   let count = 0;
-  for (let j = 0; j < string.length; j++) {
-    if (selected.includes(string.charAt(j))) {
+  for (let j = 0; j < str.length; j++) {
+    if (selected.includes(str.charAt(j))) {
       count++;
     }
   }
 
-  result[string] = count;
+  results[str] = count;
 }
 
 const shorted = Object.entries(result).sort((a, b) => b[1] - a[1])[0];
-const estimate = shorted[1] > 0 ? shorted[0] : null;
+const estimated = shorted[1] > 0 ? shorted[0] : null;
 
-console.log(`Did you mean ${estimate}?`);
+console.log(`Did you mean ${estimated}?`);
